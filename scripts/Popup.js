42da677ce306fc889f4c88de847fe42ec2502e12
Sergey Popov <sergeykms@gmail.com>
2021-02-23 16:53:32 +1000

    23_02_2021__1
    Create class Popup

---
+++ scripts/Popup.js
@@ -0,0 +1,42 @@
+class Popup {
+  constructor(popupSelector) {
+    this._popupSelector = document.querySelector(popupSelector);
+    this._popupCloseButton = document.querySelector(popupSelector).querySelector('.popup__close')
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this._popupSelector.classList.remove('popup_active');
+    }
+  }
+
+  // Функция открытия попапа с формой
+  open() {
+    this._popupSelector.classList.add('popup_active');
+    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    this._setEventListeners()
+  };
+
+  // Функция закрытия попапа с формой
+  close() {
+    this._popupSelector.classList.remove('popup_active');
+    document.removeEventListener('keydown', this._handleEscClose);
+  };
+
+  _setEventListeners() {
+    // Закрытие попапов при клике на крестик
+    this._popupCloseButton.addEventListener('click', () => {
+      this.close();
+    });
+    
+    // Закрытие попапов при клике на оверлей
+    this._popupSelector.addEventListener('click', (evt) => {
+      if (evt.target === evt.currentTarget) {
+        this.close();
+      }
+    })
+
+  }
+}
+
+export default Popup;
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,22 @@
 import initialCards from './initial-сards.js';
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
+import Popup from './Popup.js';
+
+const popupAddMesto = new Popup('.popup-add');
+const popupEditProfile = new Popup('.popup-edit');
+const popupPreviewImage = new Popup('.popup-image');
+
+
 
 const buttonFormEditOpen = document.querySelector('.profile__button-edit');
-const popupEdit = document.querySelector('.popup-edit');
+// const popupEdit = document.querySelector('.popup-edit');
 
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
 const buttonFormAddOpen = document.querySelector('.profile__button-add');
-const popupAdd = document.querySelector('.popup-add');
+// const popupAdd = document.querySelector('.popup-add');
 
 // Переменные для формы увеличенного просмотра картинки
 const popupZoomImage = document.querySelector('.popup-image');
@@ -18,44 +25,24 @@ const zoomImageImg = zoomImageForm.querySelector('.zoom-img__img');
 const zoomImageText = zoomImageForm.querySelector('.zoom-img__text');
 const zoomImageClose = zoomImageForm.querySelector('.zoom-img__close');
 
-const popupElements = document.querySelectorAll('.popup');
+// const popupElements = document.querySelectorAll('.popup');
 
 // Переменные для формы Edit
 const formEdit = document.forms.form_edit;
 const nameInput = formEdit.elements.edit_name_avatar;
 const jobInput = formEdit.elements.edit_job;
-const buttonFormEditClose = formEdit.elements.form_close;
+// const buttonFormEditClose = formEdit.elements.form_close;
 
 // Переменные для формы Add
 const formAdd = document.forms.form__add;
 const placeNameInput = formAdd.elements.add_name_mesto;
 const placeLinkInput = formAdd.elements.add_name_link;
-const buttonFormAddClose = formAdd.elements.form_close;
-
-// Закрытие попапов при нажатии клавишт Esc
-function closePopupEsc(evt) {
-  if (evt.key === "Escape") {
-    closePopup(document.querySelector('.popup_active'));
-  }
-}
-
-// Функция открытия попапа с формой
-export function openPopup(popup) {
-  popup.classList.add('popup_active');
-  document.addEventListener('keydown', closePopupEsc);
-};
-
-// Функция закрытия попапа с формой
-function closePopup(popup) {
-  popup.classList.remove('popup_active');
-  document.removeEventListener('keydown', closePopupEsc);
-};
 
 // Обработка формы Edit
 function editProfileSubmitHandler(evt) {
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup(popupEdit);
+  popupEditProfile.close();
 };
 
 // Обраблтка клика на изображении в карточке
@@ -63,8 +50,50 @@ function handleCardClick(name, link) {
   zoomImageImg.src = link
   zoomImageText.textContent = name
   zoomImageImg.alt = name;
-  openPopup(popupZoomImage)
+  popupPreviewImage.open();
 }
+
+
+// Обработка формы Add
+function addFormSubmitHandler(evt) {
+  const newMesto = {};
+  newMesto.name = placeNameInput.value;
+  newMesto.link = placeLinkInput.value;
+  addCard(createCard(newMesto));
+  popupAddMesto.close();
+};
+
+// Слушатель клика на кнопке Edit
+buttonFormEditOpen.addEventListener('click', () => {
+  clearErrorMessage(formEdit, configValidation)
+  popupEditProfile.open();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
+
+// Слушатель клика отправки формы Edit
+formEdit.addEventListener('submit', editProfileSubmitHandler);
+
+// Слушатель клика на кнопке Add
+buttonFormAddOpen.addEventListener('click', () => {
+  formAdd.reset();
+  clearErrorMessage(formAdd, configValidation)
+  popupAddMesto.open();
+});
+
+// Слушатель клика отправки формы Add
+formAdd.addEventListener('submit', addFormSubmitHandler);
+
+// Слушатель клика на кнопке закрытия формы Zoom
+zoomImageClose.addEventListener('click', () => {
+  popupPreviewImage.close();
+});
+
+
+
+//-------------------------------------------------------------------------------
+// Добавление карточек в DOM
+//-------------------------------------------------------------------------------
 
 // Поиск DOM элемента - список карточек
 const listMesto = document.querySelector('.elements__list');
@@ -86,59 +115,10 @@ initialCards.forEach(function (item) {
   addCard(createCard(item));
 });
 
-// Обработка формы Add
-function addFormSubmitHandler(evt) {
-  const newMesto = {};
-  newMesto.name = placeNameInput.value;
-  newMesto.link = placeLinkInput.value;
-  addCard(createCard(newMesto));
-  closePopup(popupAdd);
-};
 
-// Слушатель клика на кнопке Edit
-buttonFormEditOpen.addEventListener('click', () => {
-  clearErrorMessage(formEdit, configValidation)
-  openPopup(popupEdit);
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-});
-
-// Слушатель клика на кнопке закрытия формы Edit
-buttonFormEditClose.addEventListener('click', () => {
-  closePopup(popupEdit);
-});
-
-// Слушатель клика отправки формы Edit
-formEdit.addEventListener('submit', editProfileSubmitHandler);
-
-// Слушатель клика на кнопке Add
-buttonFormAddOpen.addEventListener('click', () => {
-  formAdd.reset();
-  clearErrorMessage(formAdd, configValidation)
-  openPopup(popupAdd);
-});
-
-// Слушатель клика на кнопке закрытия формы Add
-buttonFormAddClose.addEventListener('click', () => {
-  closePopup(popupAdd);
-});
-
-// Слушатель клика отправки формы Add
-formAdd.addEventListener('submit', addFormSubmitHandler);
-
-// Слушатель клика на кнопке закрытия формы Zoom
-zoomImageClose.addEventListener('click', () => {
-  closePopup(popupZoomImage);
-});
-
-// Закрытие попапов при клике на оверлей
-popupElements.forEach((item) => {
-  item.addEventListener('click', (evt) => {
-    if (evt.target === evt.currentTarget) {
-      closePopup(item);
-    }
-  });
-});
+//-------------------------------------------------------------------------------
+// Для вадидации
+//-------------------------------------------------------------------------------
 
 // Объект с настройками для FormValidator
 const configValidation = {
@@ -172,3 +152,6 @@ function clearErrorMessage(formElement, configValidation) {
     inputElement.nextElementSibling.textContent = '';
   });
 }
+
+
+
